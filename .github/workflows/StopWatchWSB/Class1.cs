78965a85d4136cf5e236e9bbd5a39029b2492874
TomaszMazurekWSB <84138512+TomaszMazurekWSB@users.noreply.github.com>
2021-06-06 22:57:33 +0200

    Add files via upload

---
+++ .github/workflows/StopWatchWSB/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatchWSB
+{
+    public class TimeCounter
+    {
+        public TimeSpan Elapsed { get; set; }
+        public DateTime StartTime { get; set; }
+        public bool isOn;
+        public void Start()
+        {
+            if (isOn == true)
+            {
+                throw new InvalidOperationException("StopWatch is already on");
+            }
+            else
+            {
+                StartTime = DateTime.Now - Elapsed;
+                isOn = true;
+            }
+        }
+
+        public void Stop()
+        {
+            if(isOn == true)
+            {
+                isOn = false;
+                Elapsed = DateTime.Now - StartTime;
+                Console.WriteLine($"Time elapsed: {Elapsed}");
+            }
+            else
+            {
+                Console.WriteLine("You haven't started the StopWatch. ");
+            }
+        }
+        public void Reset()
+        {
+            StartTime = DateTime.Now;
+            Elapsed = TimeSpan.Zero;
+        }
+    }
+}
---
+++ .github/workflows/StopWatchWSB/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace StopWatchWSB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeCounter stopWatch = new TimeCounter();
+
+            while (true)
+            {
+                Console.WriteLine("Choose action: start,stop, reset");
+                    string action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "start":
+                        stopWatch.Start();
+                        break;
+                    case "stop":
+                        stopWatch.Stop();
+                        break;
+                    case "reset":
+                        stopWatch.Reset();
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+        }
+    }
+}
